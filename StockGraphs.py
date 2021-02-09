a216c9d77e854c42a4b4c3ca2dca8ca320ce71af
JonHajdini2 <63235051+JonHajdini2@users.noreply.github.com>
2021-02-09 21:42:11 +0100

    Krijimi i figurave

--- StockGraphs.py
+++ StockGraphs.py
@@ -5,3 +5,9 @@ from matplotlib import style
 
 
 style.use('fivethirtyeight')
+
+fig= plt.figure()
+ax1=fig.addsubplot(2,2,1)
+ax2=fig.addsubplot(2,2,2)
+ax3=fig.addsubplot(2,2,3)
+ax4=fig.addsubplot(2,2,4)
