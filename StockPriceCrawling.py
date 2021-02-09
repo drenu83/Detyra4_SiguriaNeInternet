cee4e943d1724475debda7a142ec109e4bd994b4
drenu83 <45490901+drenu83@users.noreply.github.com>
2021-02-09 20:19:01 +0100

    Url me te dhenat qe perdoren

--- StockPriceCrawling.py
+++ StockPriceCrawling.py
@@ -1,3 +1,9 @@
 install requests
 
 from bs4 install BeautifulSoup
+
+
+url = ('https://finance.yahoo.com/quote/') +stock_code + ('.HK?p=') + stock_code + ('.HK&.tsrc=fin-srch')
+r=requests.get(url)
+
+print=(r.text)
