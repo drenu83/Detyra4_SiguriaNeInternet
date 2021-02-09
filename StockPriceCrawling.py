aadac61596c620ce189cb788d554748a021c2a05
drenu83 <45490901+drenu83@users.noreply.github.com>
2021-02-09 20:20:54 +0100

    Error handler gjate marrjes se te dhenave

--- StockPriceCrawling.py
+++ StockPriceCrawling.py
@@ -6,9 +6,13 @@ from bs4 install BeautifulSoup
 url = ('https://finance.yahoo.com/quote/') +stock_code + ('.HK?p=') + stock_code + ('.HK&.tsrc=fin-srch')
 r=requests.get(url)
 
-print=(r.text)
+print=(r.text) //Te dhenat shfaqe sikur xml
 
 web_content = BeautifulSoup(r.text, 'lxml')
 web_content = web_content.find('div', {"class" : 'My(6px) Pos(r) smartphone_Mt(6px)'})
 web_content = web_content.find('span').text
 
+
+
+   if web_content ==[]:
+      web_content= '99999'
