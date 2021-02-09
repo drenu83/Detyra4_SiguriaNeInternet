f3c35f2f07613bc59fd1ac9b2d6b6494e0cad8e0
JonHajdini2 <63235051+JonHajdini2@users.noreply.github.com>
2021-02-09 21:42:31 +0100

    Krijimi i animacionit

--- StockGraphs.py
+++ StockGraphs.py
@@ -11,3 +11,12 @@ ax1=fig.addsubplot(2,2,1)
 ax2=fig.addsubplot(2,2,2)
 ax3=fig.addsubplot(2,2,3)
 ax4=fig.addsubplot(2,2,4)
+
+
+def animate(i):
+    df = pd.read_csv('real time stock data.csv')
+    ys = df.iloc[1:, 2].values
+    xs = list(range(1, len(ys)))
+    ax1.clear()
+    ax1.plot(xs, ys)
+    ax1.set_title('CKH Holdings', fontsize=12)
